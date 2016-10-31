02e614c8cf162ff70898c86ab834694d3a22662d
Alex Sanchez <asanchez.dev@gmail.com>
2016-10-31 10:21:45 -0400

    adding two tests

---
+++ RevanTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Revan;
+
+namespace RevanTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSampleTime()
+        {
+            var expectedOutput = "Y RRRO ROOO YYRYYRYOOOO YYOO";
+            var input = "16:37:16";
+
+            // create a clock
+            var revan = new Revan.RevanClock();
+
+            Assert.AreEqual(expectedOutput, revan.parse(input), "The time did not parse corectly");
+        }
+
+        [TestMethod]
+        public void TestBulbSwap()
+        {
+            var expectedOutput = "R YYYO YOOO RRYRRYROOOO RROO";
+            var input = "16:37:16";
+
+            // create a clock
+            var revan = new Revan.RevanClock()
+            {
+                Bulb = new Revan.ROBulb(),
+                Accent = new Revan.QuarterAccent(new Revan.YOBulb())
+            };
+
+            Assert.AreEqual(expectedOutput, revan.parse(input), "The time did not parse corectly");
+        }
+    }
+}
